bbb783bf757712b87bc06e35779f90bd0427cf16
satendThakur <satendra@adroinfotech.in>
2024-09-14 15:09:22 +0530

    -Dark Mode and light Mode implemented

--- app/src/main/java/com/fittracker/APiService/ApiConstants.java
+++ app/src/main/java/com/fittracker/APiService/ApiConstants.java
@@ -2,7 +2,7 @@ package com.fittracker.APiService;
 
 public class ApiConstants {
 
-    public static String BASE_URL="http://13.202.198.239:8080/api/";
+    public static String BASE_URL="http://13.233.209.128:8080/api/";
     //public static String BASE_URL="http://localhost:8080/api/";
 
 }
